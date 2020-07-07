7e85b85018dadc891c9be63c73445b9470c9aac3
Sam0505 <samirpatro2001@gmail.com>
2020-07-07 22:21:06 +0530

    First commit

---
+++ src/Lesson1.cpp
@@ -0,0 +1,112 @@
+#include <iostream>
+#include <sstream>
+#define	GLEW_STATIC
+#include "GL/glew.h"
+#include "GLFW/glfw3.h"
+
+const char* title = "OpenGL Starters";
+const int height = 600;
+const int width = 800;
+bool gFullScreen = false;
+GLFWwindow* pWindow ;
+
+
+void glfw_onKey(GLFWwindow* window, int key, int scancode, int action, int mode);
+void showFPS(GLFWwindow* window);
+bool initOpenGL();
+
+int main()
+{
+	if (!initOpenGL())
+	{
+		return -1;
+	}
+	while (!glfwWindowShouldClose(pWindow))
+	{
+		showFPS(pWindow);
+		glfwPollEvents();
+		glClear(GL_COLOR_BUFFER_BIT);
+		glfwSwapBuffers(pWindow);
+	}
+	glfwTerminate();
+	return 0;
+}
+
+bool initOpenGL()
+{
+	if (!glfwInit())
+	{
+		std::cerr << "GLFW initialization failed" << std::endl;
+		return false;
+	}
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+	
+	if (gFullScreen)
+	{
+		GLFWmonitor* pMonitor = glfwGetPrimaryMonitor();
+		const GLFWvidmode* pVmode = glfwGetVideoMode(pMonitor);
+		if (pVmode != NULL)
+		{
+			pWindow = glfwCreateWindow(pVmode->width, pVmode->height, title, pMonitor, NULL);
+		}
+	}
+	else
+	{
+		pWindow = glfwCreateWindow(width, height, title, NULL, NULL);
+	}
+	if (pWindow == NULL)
+	{
+		std::cerr << "Failed to create a Window" << std::endl;
+		glfwTerminate();
+		return false;
+	}
+
+	glfwMakeContextCurrent(pWindow);
+	glfwSetKeyCallback(pWindow, glfw_onKey);
+
+	glewExperimental = GL_TRUE;
+	if (glewInit() != GLEW_OK)
+	{
+		std::cerr << "GLEW initialization failed" << std::endl;
+		return false;
+	}
+	glClearColor(0.23f, 0.38f, 0.47f, 1.0f);
+	return true;
+}
+
+void glfw_onKey(GLFWwindow* window, int key, int scancode, int action, int mode)
+{
+	if (key == GLFW_KEY_ESCAPE || action == GLFW_PRESS)
+		glfwSetWindowShouldClose(window,GL_TRUE);
+}
+
+void showFPS(GLFWwindow* window)
+{
+	static double prevSec = 0.0;
+	static int frameCount = 0;
+	double elapsedSec;
+	double currentSec = glfwGetTime();
+
+	elapsedSec = currentSec - prevSec;
+
+	//update the FPS 4times in a second
+	if (elapsedSec >= 0.25)
+	{
+		prevSec = currentSec;
+		double fps = (double)frameCount / elapsedSec;
+		double msPerFrame = 1000.0 / fps;
+
+		std::ostringstream outs;
+		outs.precision(3);
+		outs << std::fixed 
+			<<title << " "
+			<< "FPS: " << fps << " "
+			<< "FrameTime:" << msPerFrame << "(ms)";
+		glfwSetWindowTitle(window, outs.str().c_str());
+		frameCount = 0;
+	}
+	frameCount++;
+}
